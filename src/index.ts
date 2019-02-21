9ba7116d3d4c6d7655fe404eb32267450da353ce
Shinichi Tomita <shinichi.tomita@gmail.com>
2019-02-21 13:49:49 +0900

    change to create lead automatically

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,36 @@ import sqlite3 from 'sqlite3';
 import jsforce, { Connection } from 'jsforce';
 
 const SCANSNAP_DB_DIR = path.join(os.homedir(), '/Library/Application Support/PFU/ScanSnap Home/Managed/');
-const SCANSNAP_FILE_DIR = path.join(os.homedir(), '/Documents/ScanSnapHomeフォルダ');
+const SCANSNAP_FILE_DIR = path.join(os.homedir(), '/Documents/ScanSnapHome.localized');
 const SCANSNAP_DB_FILE = path.join(SCANSNAP_DB_DIR, 'ScanSnapHome.sqlite');
 const TIMESTAMP_FILE = '.timestamp';
 
 type ContentEntry = {
+  ZFAMILYNAME: string | null,
+  ZFIRSTNAME: string | null,
+  ZCOMPANY: string | null,
+  ZDEPARTMENT: string | null,
+  ZJOBTITLE: string | null,
+  ZZIPCODE: string | null,
+  ZADDRESS: string | null,
+  ZPHONENUMBER: string | null,
+  ZFAXNUMBER: string | null,
+  ZEMAIL: string | null,
   ZFILENAME: string,
-  ZEMAIL: string,
   ZCREATEDATE: number,
+};
+
+type Lead = {
+  Id?: string | null,
+  FirstName: string | null,
+  LastName: string | null,
+  Company: string | null,
+  Title: string | null,
+  PostalCode: string | null,
+  State: string | null, 
+  Phone: string | null,
+  Fax: string | null,
+  Email: string | null,
 };
 
 async function fetchRecs(db: sqlite3.Database, sql: string) {
@@ -40,14 +62,38 @@ async function main() {
   }
   try {
     const sql = `
-    SELECT ZEMAIL, ZFILENAME, ZCREATEDATE
+    SELECT
+      ZFAMILYNAME, ZFIRSTNAME, ZCOMPANY, ZDEPARTMENT, ZJOBTITLE,
+      ZZIPCODE, ZADDRESS, ZPHONENUMBER, ZFAXNUMBER, ZEMAIL, ZMEMO,
+      ZFILENAME, ZCREATEDATE
     FROM ZCONTENT
     WHERE ZCREATEDATE > ${lastDate}
     ORDER BY ZCREATEDATE DESC
     `;
     const recs = await fetchRecs(db, sql);
-    console.log('recs', recs);
-    for (const rec of recs) {
+    const leads: Lead[] = recs.map((rec) => ({
+      FirstName: rec.ZFIRSTNAME,
+      LastName: rec.ZFAMILYNAME,
+      Company: rec.ZCOMPANY,
+      Title: [
+        ...(rec.ZDEPARTMENT ? [rec.ZDEPARTMENT] : []),
+        ...(rec.ZJOBTITLE ? [rec.ZJOBTITLE] : []),
+      ].join(' '),
+      PostalCode: rec.ZZIPCODE,
+      State: rec.ZADDRESS,
+      Phone: rec.ZPHONENUMBER,
+      Fax: rec.ZFAXNUMBER,
+      Email: rec.ZEMAIL,
+    }))
+    console.log('creating leads =>', leads);
+    const rets = await conn.sobject('Lead').create(leads);
+    console.log('results =>', rets);
+    if (!Array.isArray(rets)) { return; }
+    const retEntries: IterableIterator<[number, jsforce.RecordResult]> = rets.entries();
+    for (const [i, ret] of retEntries) {
+      if (!ret.success) { continue; }
+      const id = ret.id
+      const rec = recs[i];
       const email = rec.ZEMAIL;
       const fileName = rec.ZFILENAME;
       const filePath = path.join(SCANSNAP_FILE_DIR, fileName);
@@ -59,29 +105,16 @@ async function main() {
         PathOnClient: fileName,
       };
       const contentDocumentLink = {
-        LinkedEntityId: '' as string | undefined,
+        LinkedEntityId: id,
         ContentDocumentId: '' as string | undefined,
         ShareType: 'V',
       };
-      const contact = await conn.sobject('Contact').findOne({ Email: email }).sort('CreatedDate', 'DESC');
-      if (contact) {
-        console.log('Found contact: ', (contact as any).Name);
-        contentDocumentLink.LinkedEntityId = contact.Id;
-      } else {
-        const lead = await conn.sobject('Lead').findOne({ Email: email }).sort('CreatedDate', 'DESC');
-        if (lead) {
-          console.log('Found lead: ', (lead as any).Name);
-          contentDocumentLink.LinkedEntityId = lead.Id;
-        }
-      }
-      if (contentDocumentLink.LinkedEntityId) {
-        const ret = await conn.sobject('ContentVersion').create(contentVersion);
-        if (ret.success) {
-          console.log('content version created', ret.id);
-          const version: any = await conn.sobject('ContentVersion').findOne({ Id: ret.id }, ['Id', 'ContentDocumentId']);
-          contentDocumentLink.ContentDocumentId = version.ContentDocumentId;
-          await conn.sobject('ContentDocumentLink').create(contentDocumentLink);
-        }
+      const ret2 = await conn.sobject('ContentVersion').create(contentVersion);
+      if (ret2.success) {
+        console.log('content version created', ret2.id);
+        const version: any = await conn.sobject('ContentVersion').findOne({ Id: ret2.id }, ['Id', 'ContentDocumentId']);
+        contentDocumentLink.ContentDocumentId = version.ContentDocumentId;
+        await conn.sobject('ContentDocumentLink').create(contentDocumentLink);
       }
     }
     if (recs[0]) {
